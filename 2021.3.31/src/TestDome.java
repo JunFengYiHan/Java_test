e66983a10eab7ede88489365a8721068d03a47f0
JunFengYiHan <2068619569@qq.com>
2021-03-31 23:09:40 +0800

    21.3.31

---
+++ 2021.3.31/src/TestDome.java
@@ -0,0 +1,20 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2021-03-31
+ * Time: 22:02
+ */
+
+/**
+ * /** 回车文档注释
+ */
+public class TestDome {//自动生成
+    public static void main(String[] args) {//快捷键psvm
+        double a=1.1;
+        System.out.println(a * a);//快捷键sout
+        for (int i = 0; i < 10; i++) {//快捷键fori
+            System.out.println(i);
+        }// ctrl+shift+/  块注释快捷键，取消重复同样操作
+    }//  ctrl+/  行注释，取消重复同样操作
+}
