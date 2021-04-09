b93d4554582a2a07d74e25907edd6314902e3176
JunFengYiHan <2068619569@qq.com>
2021-04-09 23:37:59 +0800

    2021.4.9

--- 2021.4.5/src/TestDome.java
+++ 2021.4.5/src/TestDome.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -47,53 +46,3 @@ public class TestDome {
         }
     }
 }
-=======
-/**
- * Created with IntelliJ IDEA.
- * Description:
- * User: 柒
- * Date: 2021-04-05
- * Time: 19:46
- */
-import java.util.Scanner;
-public class TestDome {
-    public static void main(String[] args) {
-        int i = 0;
-        int j = 0;
-        for(i = 1;i <= 9;i++){
-            for(j = 1;j <= 9;j++){
-                System.out.printf("%2d*%2d=%-3d", j, i, i*j);
-            }
-            System.out.println("");
-        }
-    }
-    public static void main2(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int age = scan.nextInt();
-        if(age<=18){
-            System.out.println("少年");
-        }else if(age<=28){
-            System.out.println("青年");
-        }else if(age<=55){
-            System.out.println("中年");
-        }else{
-            System.out.println("老年");
-        }
-    }
-    public static void main1(String[] args) {
-    Scanner scan = new Scanner(System.in);
-    int n = scan.nextInt();
-        int i=0;
-        for (i = 2; i < n; i++) {
-           if(n%i==0){
-               break;
-           }
-        }
-        if(n==2 || i==n){
-            System.out.println(n+"是素数");
-        }else{
-            System.out.println(n + "不是素数");
-        }
-    }
-}
->>>>>>> dc5ee0728d31e3ec39365b8cea49d7ae499110a9
