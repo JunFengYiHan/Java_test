773a0895fc5a0cebb29deecd62abeff16a51904b
JunFengYiHan <2068619569@qq.com>
2021-04-28 00:04:45 +0800

    21.4.27

---
+++ 2021.4.27/src/TestDome.java
@@ -0,0 +1,26 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2021-04-27
+ * Time: 19:24
+ */
+public class TestDome {
+    public static void main(String[] args) {
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addFirst(1);
+        myLinkedList.addFirst(2);
+//        myLinkedList.addFirst(3);
+//        myLinkedList.addLast(4);
+        myLinkedList.addLast(5);
+        myLinkedList.addLast(6);
+        myLinkedList.display();
+        myLinkedList.addIndex(2,18);
+        myLinkedList.addIndex(0,18);
+        myLinkedList.addIndex(6,8);
+        myLinkedList.remove(8);
+        myLinkedList.display();
+        System.out.println(myLinkedList.contains(8));
+
+    }
+}
