39eff1c1aa6e4dbbf510db78f546878be9ea8b5c
JunFengYiHan <2068619569@qq.com>
2021-06-17 01:05:47 +0800

    2021.6.17

---
+++ practice/src/leetcode/linkedlist/复制带随机指针的链表.java
@@ -0,0 +1,25 @@
+package leetcode.linkedlist;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2021-06-16
+ * Time: 21:52
+ */
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+public class 复制带随机指针的链表 {
+    public Node copyRandomList(Node head) {
+
+    }
+}
