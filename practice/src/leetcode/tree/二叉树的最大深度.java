8b3f7e843d55053585fa67f079711475dab0e4bc
JunFengYiHan <2068619569@qq.com>
2021-06-20 23:30:32 +0800

    2021.6.20

---
+++ practice/src/leetcode/tree/二叉树的最大深度.java
@@ -0,0 +1,35 @@
+//package leetcode.tree;
+//
+///**
+// * Created with IntelliJ IDEA.
+// * Description:
+// * User: 柒
+// * Date: 2021-06-20
+// * Time: 22:33
+// */
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode() {}
+//    TreeNode(int val) { this.val = val; }
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
+//public class 二叉树的最大深度 {
+//    public int maxDepth(TreeNode root) {
+//        //空节点深度为0
+//        if(root==null) {
+//            return 0;
+//        }
+//        //叶子节点深度为1
+//        if(root.left==null&&root.right==null) {
+//            return 1;
+//        }
+//        //当前节点不为空，记1，然后求左右子树深度的较大值
+//        return 1 + Math.max(maxDepth(root.left),maxDepth(root.right));
+//    }
+//}
---
+++ practice/src/leetcode/tree/对称二叉树.java
@@ -0,0 +1,47 @@
+//package leetcode.tree;
+//
+//
+///**
+// * Created with IntelliJ IDEA.
+// * Description:
+// * User: 柒
+// * Date: 2021-06-20
+// * Time: 23:07
+// */
+//class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode() {}
+//    TreeNode(int val) { this.val = val; }
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
+//public class 对称二叉树 {
+//    public boolean isSymmetric(TreeNode root) {
+//        //根节点为空，直接返回true
+//        if(root==null) {
+//            return true;
+//        }
+//        return isMirror(root.left,root.right);
+//    }
+//    public boolean isMirror(TreeNode t1, TreeNode t2) {
+//        //左右都为空，说明对称
+//        if(t1==null&&t2==null) {
+//            return true;
+//        }
+//        //一个为空一个不为空说明不对称
+//        if(t1==null||t2==null) {
+//            return false;
+//        }
+//        //对应节点的值不相等，也不对称
+//        if(t1.val!=t2.val) {
+//            return false;
+//        }
+//        //左右子树是否为镜像
+//        return isMirror(t1.left,t2.right) && isMirror(t1.right,t2.left);
+//    }
+//}
