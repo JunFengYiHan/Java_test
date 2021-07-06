dcdcab35e7b22d7c25847c4bc874192929ac3b7d
JunFengYiHan <2068619569@qq.com>
2021-07-06 22:18:35 +0800

    2021.7.6整理+刷题打卡

--- practice/src/leetcode/tree/二叉树的层序遍历/Solution.java
+++ practice/src/leetcode/tree/二叉树的层序遍历/Solution.java
@@ -26,19 +26,22 @@ class TreeNode {
 public class Solution {
     List<List<Integer>> result = new ArrayList<>();
     public List<List<Integer>> levelOrder(TreeNode root) {
+        //根节点不为空，遍历整棵树
         if(root!=null) {
             helper(root,0);
         }
         //result.remove(result.size()-1);方法1
         return result;
     }
-
+    //tier 表示当前深度，也就是对应二维数组的行数
     private void helper(TreeNode root, int tier) {
         //方法2
         if(tier==result.size()){
+            //添加一行
             result.add(new ArrayList<>());
         }
-        if(root==null) return;
+        //if(root==null) return;方法一
+        //递归的一个前序遍历
         result.get(tier).add(root.val);
         if(root.left!=null) {
             helper(root.left,tier+1);
