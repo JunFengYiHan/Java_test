4e6905bf27a12aa56d0a9d1ca70da4a647cd791b
JunFengYiHan <2068619569@qq.com>
2021-08-15 21:15:38 +0800

    2021.8.15刷题打卡

--- practice/src/数据结构/map/MyHashMap.java
+++ practice/src/数据结构/map/MyHashMap.java
@@ -7,6 +7,7 @@ package 数据结构.map;
  * Date: 2021-08-04
  * Time: 16:39
  */
-public class MyHashMap {
+public class MyHashMap<T> {
+    T[] elem;
 
 }
---
+++ project/src/图书管理系统/TestDemo.java
@@ -0,0 +1,14 @@
+package 图书管理系统;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2021-08-13
+ * Time: 9:31
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+
+    }
+}
