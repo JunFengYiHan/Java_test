4627a9ebc8202ac9dfb42e31a53345ab0ea6cdb5
JunFengYiHan <2068619569@qq.com>
2022-07-13 00:00:18 +0800

    每日练习

---
+++ practice/src/leetcode/每日一题/奇数值单元格的数目/Solution.java
@@ -0,0 +1,40 @@
+package leetcode.每日一题.奇数值单元格的数目;
+
+import java.util.Arrays;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2022/7/12
+ * Time: 23:41
+ */
+public class Solution {
+    public int oddCells(int m, int n, int[][] indices) {
+        int[][] arr = new int[m][n];
+        int count = 0;
+        for (int i = 0; i < indices.length; i++) {
+            int row = indices[i][0];
+            int col = indices[i][1];
+            for (int j = 0; j < m; j++) {
+                arr[row][j] += 1;
+            }
+            for (int j = 0; j < n; j++) {
+                arr[j][col] += 1;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] % 2 != 0){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{0, 0}, {0, 0}};
+        System.out.println(new Solution().oddCells(1, 1, arr));
+    }
+}
