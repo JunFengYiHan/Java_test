b190af4b4228320d8116603a89b23d42c0f80fe5
JunFengYiHan <2068619569@qq.com>
2022-07-17 23:36:54 +0800

    每日练习

--- practice/src/笔试练习/查找两个字符串ab中的最长公共子串/Main.java
+++ practice/src/笔试练习/查找两个字符串ab中的最长公共子串/Main.java
@@ -12,10 +12,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
+        while (sc.hasNextLine()) {
             String str1 = sc.nextLine();
             String str2 = sc.nextLine();
 
         }
+
+    }
+
+    private static int[] getNext(String str) {
+        char[] arr = str.toCharArray();
+        int[] ret = new int[arr.length];
+        ret[0] = -1;
+        int j = 0;
+        int k = -1;
+        while (j < arr.length - 1                        ) {
+            if (k == -1 || arr[j] == arr[k]) {
+                ret[++j] = ++k;
+            } else {
+                k = ret[k];
+            }
+        }
+        return ret;
     }
 }
