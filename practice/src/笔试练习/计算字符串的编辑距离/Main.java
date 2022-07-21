6928d9ca73511d94f2e59d909a85f3cc15c83936
JunFengYiHan <2068619569@qq.com>
2022-07-21 21:55:45 +0800

    每日练习

---
+++ practice/src/笔试练习/计算字符串的编辑距离/Main.java
@@ -0,0 +1,23 @@
+package 笔试练习.计算字符串的编辑距离;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2022/7/21
+ * Time: 21:39
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = scanner.nextLine();
+        String str2 = scanner.nextLine();
+        int count = Math.abs(str1.length()-str2.length());//长度差
+
+        for (int i = 0; i < ; i++) {
+
+        }
+    }
+}
