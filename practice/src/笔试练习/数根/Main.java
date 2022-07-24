8a004f0b77ab28453800ed4b4c527e00a79989fe
JunFengYiHan <2068619569@qq.com>
2022-07-24 23:54:56 +0800

    每日练习

---
+++ practice/src/笔试练习/数根/Main.java
@@ -0,0 +1,33 @@
+package 笔试练习.数根;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2022/7/24
+ * Time: 23:33
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String  n = scanner.next();
+            char ret = func(n);
+            System.out.println(ret);
+        }
+    }
+    //计算一个数的数根
+    private static char func(String str) {
+        Stack<Character> stack = new Stack<>();
+        char[] arr = str.toCharArray();
+        while(stack.)
+    }
+    static class Node{
+
+    }
+}
---
+++ practice/src/笔试练习/星际密码/Main.java
@@ -0,0 +1,11 @@
+package 笔试练习.星际密码;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2022/7/24
+ * Time: 23:51
+ */
+public class Main {
+}
