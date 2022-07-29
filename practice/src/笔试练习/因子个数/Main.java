aa5044ea0d6c5d5de700a5afad8bf79212d92b48
JunFengYiHan <2068619569@qq.com>
2022-07-29 23:55:13 +0800

    每日练习

---
+++ practice/src/笔试练习/因子个数/Main.java
@@ -0,0 +1,11 @@
+package 笔试练习.因子个数;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2022/7/29
+ * Time: 23:54
+ */
+public class Main {
+}
---
+++ practice/src/笔试练习/最难的问题/Main.java
@@ -0,0 +1,11 @@
+package 笔试练习.最难的问题;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2022/7/29
+ * Time: 23:55
+ */
+public class Main {
+}
