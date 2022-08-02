7c75a6435fcaf93b264e7ab88918f2bb1c3b3f8a
JunFengYiHan <2068619569@qq.com>
2022-08-03 00:29:00 +0800

    每日练习

--- practice/src/笔试练习/客似云来/Main.java
+++ practice/src/笔试练习/客似云来/Main.java
@@ -1,5 +1,10 @@
 package 笔试练习.客似云来;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created with IntelliJ IDEA.
  * Description:
@@ -8,7 +13,39 @@ package 笔试练习.客似云来;
  * Time: 23:57
  */
 public class Main {
-    public static void main(String[] args) {
+    static List<BigDecimal> list = new ArrayList<>();
 
+    static {
+        list.add(new BigDecimal("1"));
+        list.add(new BigDecimal("1"));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int from = scanner.nextInt();
+            int to = scanner.nextInt();
+            if (to > list.size()) {
+                fibonacci(to);
+            }
+            BigDecimal sum = new BigDecimal("0");
+            for (int i = from - 1; i < to; i++) {
+                sum = sum.add(list.get(i));
+            }
+            System.out.println(sum);
+        }
+    }
+
+    static BigDecimal f1 = new BigDecimal("1");
+    static BigDecimal f2 = new BigDecimal("1");
+
+    public static void fibonacci(int n) {
+        //不需要重置继续往下求
+        while (n > list.size()) {
+            BigDecimal f3 = f1.add(f2);
+            f1 = f2;
+            f2 = f3;
+            list.add(f3);
+        }
     }
 }
