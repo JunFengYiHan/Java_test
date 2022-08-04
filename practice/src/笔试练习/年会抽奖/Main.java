6953e4a08e445d5348abce3afb594e2e681ae07f
JunFengYiHan <2068619569@qq.com>
2022-08-04 23:56:42 +0800

    每日练习

---
+++ practice/src/笔试练习/年会抽奖/Main.java
@@ -0,0 +1,30 @@
+package 笔试练习.年会抽奖;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2022/8/4
+ * Time: 23:34
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            double ret = getProbability(n);
+            System.out.printf("%.2f%%", ret * 100);
+        }
+    }
+
+    public static double getProbability(int n) {
+        double sum = 1.0;
+        //求每个人都没获奖
+        for (int i = 0; i < n - 1; i++) {
+            sum *= (n - 1.0 - i) / (n - i);
+        }
+        return sum;
+    }
+}
---
+++ practice/src/笔试练习/抄送列表/Main.java
@@ -0,0 +1,17 @@
+package 笔试练习.抄送列表;
+
+import java.util.Scanner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2022/8/4
+ * Time: 23:34
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
