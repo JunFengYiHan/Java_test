a479198c9564fcbb91e77defadacd1af95a87f5b
JunFengYiHan <2068619569@qq.com>
2022-08-08 22:28:45 +0800

    每日练习

---
+++ practice/src/笔试练习/红与黑/Main.java
@@ -0,0 +1,14 @@
+package 笔试练习.红与黑;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2022/8/8
+ * Time: 22:27
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
