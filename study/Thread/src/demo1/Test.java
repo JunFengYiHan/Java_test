9862db63451a1891b5f3383d351fb89fd8c41990
JunFengYiHan <2068619569@qq.com>
2023-07-09 21:22:13 +0800

    test

---
+++ study/Thread/src/demo1/Test.java
@@ -0,0 +1,15 @@
+package demo1;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2023/7/9
+ * Time: 21:18
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
