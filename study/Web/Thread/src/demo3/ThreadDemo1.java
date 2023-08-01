cfeb72e352c96b9b3e361f841f50d34dcbe759b6
JunFengYiHan <2068619569@qq.com>
2023-08-01 23:14:33 +0800

    Thread

---
+++ study/Web/Thread/src/demo3/ThreadDemo1.java
@@ -0,0 +1,18 @@
+package demo3;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2023/8/1
+ * Time: 22:05
+ */
+
+public class ThreadDemo1 {
+    public static void main(String[] args) {
+        AtomicInteger num = new AtomicInteger(10);
+        num
+    }
+}
