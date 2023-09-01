e6ee0b11a1ebd61bf660d71d074dd849635e06c6
JunFengYiHan <2068619569@qq.com>
2023-09-02 00:36:06 +0800

    test

---
+++ study/Web/test_maven/src/main/java/Test.java
@@ -0,0 +1,16 @@
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: 柒
+ * Date: 2023/9/2
+ * Time: 0:26
+ */
+
+public class Test {
+    DataSource dataSource = new MysqlDataSource();
+
+}
